961e69c5d0835d1fee377e466028ba559ff1298d
Noah Stevens <ss.noah@gmail.com>
2021-02-03 17:50:32 -0800

    Added Basic Ai

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -6,10 +6,14 @@ const player = (name, piece) =>{
 }
 const gameBoard = (() =>{
     let board = new Array(9);
-    let player1, player2, flop;
+    let validChoice = [0,1,2,3,4,5,6,7,8];
+    let player1, player2, flop, aiLevel;
     const getBoard = (x) => board[x];
     const setBoard = (x, value) => board[x] = value;
-    const reset = () => board = new Array(9);
+    const reset = () => {
+        board = new Array(9);
+        validChoice = [0,1,2,3,4,5,6,7,8];
+    }
     const playerAddMarks = (x) =>{
        return function (){
         gamestate = gameOver();
@@ -26,11 +30,18 @@ const gameBoard = (() =>{
             if(gamestate != ''){
                 displayController.showWinner(gamestate);
             }
+            validChoice.splice(validChoice.indexOf(x), 1);
+            console.log(validChoice);
         }
         else if(gamestate == ''){
                 console.log("Invalid Location");
             }
         else console.log('Winner already declared');
+        if(aiLevel >0 && !flop && gamestate == ''){
+            if(aiLevel == 1){
+                ranAi();
+            }
+        }
        };
     };
     const gameOver = () => {
@@ -97,10 +108,21 @@ const gameBoard = (() =>{
             return "You Tied!";
         }
     };
-    const createPlayers = (name1, piece1, name2, piece2) => {
+    const createPlayers = (name1, piece1, name2, piece2, aiLvl) => {
         player1 = player(name1, piece1);
         player2 = player(name2, piece2);
-        flop = true;
+        flop = piece1 == 'X';
+        aiLevel = aiLvl;
+        if(aiLevel >0 && !flop){
+            console.log('hit');
+            ranAi();
+        }
+    }
+    const ranAi = () =>{
+        let move = validChoice[Math.floor(Math.random() * validChoice.length)];
+        console.log(move);
+        let aimove =playerAddMarks(move);
+        aimove();
     }
     return {getBoard, setBoard, reset, playerAddMarks, gameOver, createPlayers};
 })();
@@ -141,7 +163,20 @@ const displayController = (() =>{
         const player1 = document.getElementById("player1");
         const player2 = document.getElementById("player2");
         const startDiv = document.getElementById("start");
-        gameBoard.createPlayers(player1.value, 'X', player2.value, "O");
+        const piece = document.getElementsByName('Mark');
+        const opponent = document.getElementsByName('opponent');
+        let aiLvl =0;
+        for (let i = 0; i < opponent.length; i++) {
+            console.log(opponent[i]);
+            if(opponent[i].checked == true){
+                console.log(aiLvl);
+                aiLvl = opponent[i].value;
+                console.log(aiLvl);
+                break;
+            }
+        }
+        if(piece[0].checked)gameBoard.createPlayers(player1.value, 'X', player2.value, "O", aiLvl);
+        else gameBoard.createPlayers(player1.value, 'O', player2.value, "X", aiLvl);
         startDiv.style.display = 'none';
         update();
     }
